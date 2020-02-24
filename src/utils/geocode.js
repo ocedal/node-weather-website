96487ba3f1237484721e68eccd9e5c26b1809f64
Octavio Guzman <guzaoctavio@gmail.com>
2020-02-24 15:31:51 -0800

    Initial commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoib2N0YXZpb2d1emEiLCJhIjoiY2s2d3czandiMGNpMTNlbzhscm9sM21lMCJ9.gQEvBFlQG2W4pH3gq0cLOg&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            console.log(body.features);
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
